827a70675cee53206b80ae12b4f9fd6d9c4ad74b
John Blossom <jblossom.web@gmail.com>
2014-11-06 17:17:46 -0500

    provided skeleton

---
+++ main.php
@@ -0,0 +1,35 @@
+<?php
+/* Read file and a create an object array of shapes */
+
+/* Get the array index of the shape with ID that was passed in and write to screen */
+
+/* Sort the shapes by area and write the results to the screen */
+
+/* Get the new array index of the shape with ID that was passed in and write to screen */
+
+/* Write any helper methods necessary to support the program */
+
+/**
+* Returns the array index of the instance in the list to the caller.
+* This method takes in the shape instance to compare it with the
+* shapes in the $shapes array and to find the index of that shape
+* within the array.
+*
+* @param Shape $instance Individual Shape Instance
+* @param Shape[] $shapes All Shapes
+* @return int Shapes array index
+*/
+function findShapeIndex($instance, $shapes) {
+
+// implementation
+}
+
+/**
+* Sorts the list of shapes by area
+*
+* @param Shape[] $shapes Collection of shapes
+*/
+function sortByArea(&$shapes) {
+
+// implementation
+}
